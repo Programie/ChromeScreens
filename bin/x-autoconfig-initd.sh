0868ed5dc88a72d5744e49fce16c3c078b2875da
Michael Wieland <github@selfcoders.com>
2014-01-24 18:05:28 +0100

    Fixed skipping x-autoconfig in update-rc.d

--- bin/x-autoconfig-initd.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-
-if [ ! -f /etc/X11/xorg.conf ]; then
-	/usr/bin/x-autoconfig
-fi
-update-rc.d x-autoconfig remove
-rm $0
--- configure.sh
+++ configure.sh
@@ -63,13 +63,23 @@ TryExec=$SCRIPTPATH/bin/session.sh
 Type=XSession
 EOF
 
+echo "Writing x-autoconfig init.d script..."
+cat > /etc/init.d/x-autoconfig << EOF
+#! /bin/sh
+
+if [ ! -f /etc/X11/xorg.conf ]; then
+	/usr/bin/x-autoconfig
+fi
+update-rc.d x-autoconfig remove
+rm \$0
+EOF
+chmod +x /etc/init.d/x-autoconfig
+update-rc.d x-autoconfig defaults
+
 echo "Generating symbolic links..."
-ln -fs $SCRIPTPATH/bin/x-autoconfig-initd.sh /etc/init.d/x-autoconfig
+ln -fs $SCRIPTPATH/bin/x-autoconfig-initd.sh 
 ln -fs $SCRIPTPATH/conf/lightdm.conf /etc/lightdm.conf
 ln -fs $SCRIPTPATH/conf/disable-beep.conf /etc/modprobe.d/disable-beep.conf
-
-echo "Registering x-autoconfig init.d script..."
-update-rc.d x-autoconfig defaults
 
 if [ ! -f $SCRIPTPATH/conf/suspend-wakeup.conf ]; then
 	echo "Copying example suspend-wakeup configuration..."
